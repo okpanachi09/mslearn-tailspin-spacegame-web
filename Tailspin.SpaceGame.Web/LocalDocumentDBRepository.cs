53b40b6f8df51ad87c74c3f7ad4038030c459b4a
kingabel <yakubuabel9@gmail.com>
2020-04-21 22:31:01 +0200

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -58,13 +58,13 @@ namespace TailSpin.SpaceGame.Web
         )
         {
             var result = _items.AsQueryable()
-                .Where(queryPredicate) // filter
-                .OrderByDescending(orderDescendingPredicate) // sort
-                .Skip(page * pageSize) // find page
-                .Take(pageSize - 1) // take items
-                .AsEnumerable(); // make enumeratable
+            .Where(queryPredicate) // filter
+            .OrderByDescending(orderDescendingPredicate) // sort
+            .Skip(page * pageSize) // find page
+            .Take(pageSize) // take items
+            .AsEnumerable(); // make enumeratable
 
-            return Task<IEnumerable<T>>.FromResult(result);
+        return Task<IEnumerable<T>>.FromResult(result);
         }
 
         /// <summary>
